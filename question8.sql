a1f4847d626ad92071e083b93416c5c696f0767e
adityababar07 <adityababar715@gmail.com>
2021-03-18 17:22:27 +0530

    initial changes

--- question8.sql
+++ question8.sql
@@ -10,6 +10,11 @@ check(gender in ( 'm','f'))
 );
 
 insert into test.stud_info values(1001, 'Aditya', 'Sangli', 17, 'm');
+insert into test.stud_info values(1002, 'Rakhi', 'Pune', 18, 'f;');
+insert into test.stud_info values(1003, 'Benny', 'Kolhapur', 18, 'm') ;
+insert into test.stud_info values(1004, 'Kedarnath', 'Sangli', 19, 'm');
+ insert into test.stud_info values(1005, 'Pranav', 'Mumbai', 17, 'm') ;
+ insert into test.stud_info values(1006, 'Sejal', 'Satara', 18, 'f') ;
 select * from test.stud_info;
+--showing the structure of the table
 Describe test.stud_info;
-
---
+++ question9.sql
@@ -0,0 +1,12 @@
+use test;
+create table stud_marks
+(
+reg_no int primary key not null,
+CS_marks int not null,
+Maths_marks int not null,
+Phy_marks int not null,
+Chem_marks int not null
+);
+
+insert into 
+
